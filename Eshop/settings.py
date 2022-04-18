b3903d8ad441aa50ec47af03ba3f83d8f3df6815
Qn4l <Kunal.chauhan19@comp.sce.edu.in>
2022-04-18 23:59:01 +0530

    allowed host

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-oc=q@!gc$!kd$-tx8@w8hna&7c=$vtc&urfrkjr%b&8j2_-8fx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['eshopie.azurewebsites.net']
 
 
 # Application definition
