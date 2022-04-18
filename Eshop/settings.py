e467b1d8d57392a50cd7d8ad64a2c26b43ddf717
Qn4l <Kunal.chauhan19@comp.sce.edu.in>
2022-04-19 00:18:27 +0530

    WhiteNoiseMiddleware

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -122,6 +123,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = "/images/products/download/"
 MEDIA_ROOT = BASE_DIR
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
